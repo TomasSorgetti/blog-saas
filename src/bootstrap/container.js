189cd67039a7acf01cb00c21b3b17943ca85439e
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-06 19:54:59 -0300

    refactor: comment routes removed

--- src/bootstrap/container.js
+++ src/bootstrap/container.js
@@ -7,7 +7,6 @@ import AuthController from "../interfaces/http/controllers/auth.controller.js";
 import UserController from "../interfaces/http/controllers/user.controller.js";
 import ArticleController from "../interfaces/http/controllers/article.controller.js";
 import CategoryController from "../interfaces/http/controllers/category.controller.js";
-import CommentController from "../interfaces/http/controllers/comment.controller.js";
 
 export default class Container {
   constructor(config = {}) {
@@ -26,7 +25,6 @@ export default class Container {
     this.controllers.userController = new UserController();
     this.controllers.articleController = new ArticleController();
     this.controllers.categoryController = new CategoryController();
-    this.controllers.commentController = new CommentController();
   }
 
   getDependencies() {
--- src/interfaces/http/routes/main.router.js
+++ src/interfaces/http/routes/main.router.js
@@ -4,7 +4,6 @@ import AuthRouter from "./auth.router.js";
 import UserRouter from "./user.router.js";
 import ArticleRouter from "./article.router.js";
 import CategoryRouter from "./category.router.js";
-import CommentRouter from "./comment.router.js";
 
 class MainRouter {
   constructor(dependencies = {}) {
@@ -25,15 +24,11 @@ class MainRouter {
     const categoryRouter = new CategoryRouter({
       categoryController: dependencies.controllers.categoryController,
     });
-    const commentRouter = new CommentRouter({
-      commentController: dependencies.controllers.commentController,
-    });
 
     this.router.use("/auth", authRouter.getRouter());
     this.router.use("/users", userRouter.getRouter());
     this.router.use("/articles", articleRouter.getRouter());
     this.router.use("/categories", categoryRouter.getRouter());
-    this.router.use("/comments", commentRouter.getRouter());
   }
 
   getRouter() {
