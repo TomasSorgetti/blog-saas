69951bb03c3d9fff58bc992b4fb45395908bb73a
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-06 19:38:46 -0300

    feat: user rutes and controllers added

--- src/interfaces/http/controllers/user.controller.js
+++ src/interfaces/http/controllers/user.controller.js
@@ -3,9 +3,99 @@ import successResponse from "../utils/success-response.js";
 export default class UserController {
   constructor() {}
 
-  async getAll(req, res, next) {
+  async profile(req, res, next) {
     try {
-      const data = "data";
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateProfile(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async changePassword(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async changeEmail(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteProfile(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getAllUsers(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getUserById(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createUser(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteUser(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async changeRole(req, res, next) {
+    try {
+      const data = {};
+      return successResponse(res, data, "Users retrieved successfully", 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteSessions(req, res, next) {
+    try {
+      const data = {};
       return successResponse(res, data, "Users retrieved successfully", 200);
     } catch (error) {
       next(error);
--- src/interfaces/http/routes/user.router.js
+++ src/interfaces/http/routes/user.router.js
@@ -11,7 +11,82 @@ export default class UserRouter {
   }
 
   setupRoutes() {
-    this.router.get("/", this.controller.getAll.bind(this.controller));
+    /**
+     * @GET /api/users/me
+     */
+    this.router.get("/me", this.controller.profile.bind(this.controller));
+    /**
+     * @PATCH /api/users/me
+     */
+    this.router.patch(
+      "/me",
+      this.controller.updateProfile.bind(this.controller)
+    );
+    /**
+     * @PATCH /api/users/me/password
+     */
+    this.router.patch(
+      "/me/password",
+      this.controller.changePassword.bind(this.controller)
+    );
+    /**
+     * @PATCH /api/users/me/email
+     */
+    this.router.patch(
+      "/me/email",
+      this.controller.changeEmail.bind(this.controller)
+    );
+    /**
+     * @DELETE /api/users/me
+     */
+    this.router.delete(
+      "/me",
+      this.controller.deleteProfile.bind(this.controller)
+    );
+
+    //* admin
+    /**
+     * @GET /api/users/all
+     */
+    this.router.get(
+      "/admin",
+      this.controller.getAllUsers.bind(this.controller)
+    );
+    /**
+     * @GET /api/users/admin/:id
+     */
+    this.router.get(
+      "/admin/:id",
+      this.controller.getUserById.bind(this.controller)
+    );
+    /**
+     * @POST /api/users/admin
+     */
+    this.router.post(
+      "/admin",
+      this.controller.createUser.bind(this.controller)
+    );
+    /**
+     * @DELETE /api/users/admin/:id
+     */
+    this.router.delete(
+      "/admin/:id",
+      this.controller.deleteUser.bind(this.controller)
+    );
+    /**
+     * @PATCH /api/users/admin/:id/roles
+     */
+    this.router.patch(
+      "/admin/:id/roles",
+      this.controller.changeRole.bind(this.controller)
+    );
+    /**
+     * @DELETE /api/users/admin/:id/tokens
+     */
+    this.router.delete(
+      "/admin/:id/tokens",
+      this.controller.deleteSessions.bind(this.controller)
+    );
   }
 
   getRouter() {
