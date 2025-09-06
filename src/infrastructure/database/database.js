a162320e874267d5f16faf20fc084b14114b7164
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-06 18:00:53 -0300

    first commit

---
+++ src/infrastructure/database/database.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import User from "./schemas/user.schema.js";
+
+export const connectDB = async (dbUrl) => {
+  if (!dbUrl) {
+    throw new Error("No db url provided");
+  }
+  if (mongoose.connection.readyState >= 1) {
+    console.log("MongoDB ya conectado");
+    return;
+  }
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 10,
+      minPoolSize: 2,
+    });
+    console.log("MongoDB connected");
+    return {
+      mongoose,
+      models: {
+        User,
+      },
+    };
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    throw err;
+  }
+};
---
+++ src/infrastructure/database/repositories/user.repository.js
@@ -0,0 +1,60 @@
+class UserRepository {
+  constructor(config = {}) {
+    this.model = config.db?.models.User;
+    if (!this.model) throw new Error("User model not provided");
+  }
+
+  async findById(id) {
+    try {
+      const user = await this.model.findById(id).lean();
+      if (!user) throw new Error("User not found");
+      return user;
+    } catch (error) {
+      throw new Error(`Error finding user: ${error.message}`);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.model.find().lean();
+    } catch (error) {
+      throw new Error(`Error fetching users: ${error.message}`);
+    }
+  }
+
+  async create(data) {
+    try {
+      const user = new this.model(data);
+      return await user.save();
+    } catch (error) {
+      throw new Error(`Error creating user: ${error.message}`);
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const user = await this.model
+        .findByIdAndUpdate(id, data, {
+          new: true,
+          runValidators: true,
+        })
+        .lean();
+      if (!user) throw new Error("User not found");
+      return user;
+    } catch (error) {
+      throw new Error(`Error updating user: ${error.message}`);
+    }
+  }
+
+  async delete(id) {
+    try {
+      const user = await this.model.findByIdAndDelete(id).lean();
+      if (!user) throw new Error("User not found");
+      return { id };
+    } catch (error) {
+      throw new Error(`Error deleting user: ${error.message}`);
+    }
+  }
+}
+
+export default UserRepository;
