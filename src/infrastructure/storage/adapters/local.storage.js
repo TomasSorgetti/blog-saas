280f40970205811e32dcd5ef5a62ee93a0d2c993
TomasSorgetti <tomassorgetti456@gmail.com>
2025-10-17 12:04:10 -0300

    fix: local storage path updated to full uri

--- src/infrastructure/storage/adapters/local.storage.js
+++ src/infrastructure/storage/adapters/local.storage.js
@@ -3,25 +3,29 @@ import { promises as fs } from "fs";
 import path from "path";
 
 export class LocalStorageAdapter extends StoragePort {
+  #apiUrl;
+  #uploadDir;
+
   constructor(config) {
     super();
-    this.uploadDir = config.env.UPLOAD_DIR || "./uploads";
+    this.#uploadDir = config.env.UPLOAD_DIR || "./uploads";
+    this.#apiUrl = config.env.API_URL;
   }
 
   async upload(file, filename) {
-    const filePath = path.join(this.uploadDir, filename);
+    const filePath = path.join(this.#uploadDir, filename);
     await fs.writeFile(filePath, file.buffer);
     return filename;
   }
 
   async delete(filename) {
-    const filePath = path.join(this.uploadDir, filename);
+    const filePath = path.join(this.#uploadDir, filename);
     await fs.unlink(filePath).catch((err) => {
       if (err.code !== "ENOENT") throw err;
     });
   }
 
   getUrl(filename) {
-    return `/uploads/${filename}`;
+    return `${this.#apiUrl}/uploads/${filename}`;
   }
 }
